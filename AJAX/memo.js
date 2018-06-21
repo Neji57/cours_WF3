8706dbf981ef687b34de4f0b73ca583d6c2d7f1e
Picobuu <steve.toffano@gmail.com>
2018-06-21 16:03:13 +0200

    Création de memo.js

--- AJAX/memo.js
+++ AJAX/memo.js
@@ -2,3 +2,11 @@
 var xhr = new XMLHttpRequest;
 xhr.open(METHOD, URL, ASYNC); // Prépare la requête
 xhr.send();
+xhr.onreadystatechange = function()
+{
+	this.status; // Statut de la réponse serveur, 200 = OK
+	this.readyState; // Etat de la requête 4 = requête finie, le serveur à répondu
+	this.responseText; // Contenu de la réponse
+	JSON.parse(STRING); // Transforme une string en objet JS
+}
+xhr.send();
