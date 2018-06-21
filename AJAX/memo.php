71e1ed08f35086929e4da4368506c7d5ebc01c63
Picobuu <steve.toffano@gmail.com>
2018-06-21 16:26:22 +0200

    Création de memo.js

---
+++ AJAX/memo.php
@@ -0,0 +1,6 @@
+<?php
+
+header('Content-Type: application/json'); // Indique dans le header que la réponse sera sous forme de JSON
+
+json_encode( array );
+json_decode( string, /*true: retourne un array */ ); // retourne un objet PHP à partir d'une chaîne JSON
