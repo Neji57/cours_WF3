28a4e9efa2c05965df3d377a6011958e83045d2e
Picobuu <steve.toffano@gmail.com>
2018-06-22 11:18:24 +0200

    Update

--- PHP-Objet/exemple-1/index2.php
+++ PHP-Objet/exemple-1/index2.php
@@ -2,6 +2,7 @@
 
 class Math
 {
+	const PI = 3.14;
 	public static function sqrt($value)
 	{
 		return sqrt($value);
@@ -12,5 +13,5 @@ class Math
 // echo $objMath->sqrt(64);
 
 echo Math::sqrt(64);
-echo '<br';
+echo '<br>';
 echo Math::PI;
