011c1f571132bf930db4633aa49f77b103c4d671
Picobuu <steve.toffano@gmail.com>
2018-06-22 16:14:51 +0200

    Update

--- PHP-Objet/exemple-5/index.php
+++ PHP-Objet/exemple-5/index.php
@@ -1,5 +1,15 @@
 <?php require_once('include/header.php'); ?>
 
+<?php 
 
+require_once('form/form.php'); 
+
+$myForm = new Form ("login");
+
+
+
+
+
+?>
 
 <?php require_once('include/footer.php'); ?>
