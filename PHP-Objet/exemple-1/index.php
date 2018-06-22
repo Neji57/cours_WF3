c1bcd06864b622176374feb6f27c0a914081e895
Picobuu <steve.toffano@gmail.com>
2018-06-22 09:45:57 +0200

    Update

--- PHP-Objet/exemple-1/index.php
+++ PHP-Objet/exemple-1/index.php
@@ -1,16 +1,33 @@
 <?php
 
-class Article
+class Article // Création de la classe
 {
-	public $titre;
+	public $titre; // Création de la variable $titre dans la classe Article
+
+	public function getTitre()
+	{
+		return $this->titre;
+	}
+
+	public function setTitre()
+	{
+		echo $titre . '</br>';
+		echo $this->titre;
+	}
 }
 
+//$monArticle = new Article; // création d'un objet
+//$monArticle->titre = "Mon super article"; // On ajoute une chaîne dans $titre
+
+//echo $monArticle->titre;
+
+//$article2 = new Article; // Création d'un nouvel objet
+//$article2->titre = "Encore un !"; // Ajout d'une chaîne dans la variable $titre
+// La variable $titre de L'objet $article2 est indépendante de $monArticle (son contenu n'est pas remplacé)
+
+//echo $article2->titre;
+
 $monArticle = new Article;
-$monArticle->titre = "Mon super article";
-
-echo $monArticle->titre;
-
 $article2 = new Article;
-$article2->titre = "Encore un !";
 
-echo $article2->titre;
+$monArticle = setTitre("Nouvel article");
