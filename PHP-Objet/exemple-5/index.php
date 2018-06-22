60cb7d8826c827aab825cb990185a844cc0b9de3
Picobuu <steve.toffano@gmail.com>
2018-06-22 16:46:44 +0200

    Update

--- PHP-Objet/exemple-5/index.php
+++ PHP-Objet/exemple-5/index.php
@@ -1,8 +1,10 @@
 <?php 
 require_once('include/header.php'); 
-require_once('form/form.php'); 
-
+require_once('form/form.php');
+require_once('form/textItem.php');
+ 
 $myForm = new Form ("login", "POST", "", array("class" => "form", "id" => "login-form"));
+$myForm->addItem(new TextItem("username"));
 echo $myForm->createView();
 
 
