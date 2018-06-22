e4ac9e7be05a77e3744c25367c77a5002346b06f
Picobuu <steve.toffano@gmail.com>
2018-06-22 12:38:52 +0200

    Update

--- PHP-Objet/exemple-2/DVD.php
+++ PHP-Objet/exemple-2/DVD.php
@@ -4,5 +4,35 @@ require_once('produit.php');
 
 class DVD extends Produit
 {
-	
+	private $acteurs;
+	private $zone;
+
+	public function __construct()
+	{
+		$this->poids = 110; // 110 gr
+	}
+
+	public function getActeurs()
+	{
+		return $this->acteurs;
+	}
+
+	public function setActeurs(array $acteurs)
+	{
+		$this->acteurs = $acteurs;
+
+		return $this;
+	}
+
+	public function getZone()
+	{
+		return $this->zone;
+	}
+
+	public function setZone($zone)
+	{
+		$this->zone = $zone;
+
+		return $this;
+	}
 }
--- PHP-Objet/exemple-2/index.php
+++ PHP-Objet/exemple-2/index.php
@@ -1,3 +1,9 @@
 <?php
 
 require_once('DVD.php');
+
+$monDVD = new DVD();
+$monDVD->setPrix(12.99);
+
+echo $monDVD->getPrix();
+echo $monDVD->getPoids();
