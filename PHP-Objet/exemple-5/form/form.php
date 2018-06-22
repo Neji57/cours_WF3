788039d07a5ae67ddf54bcea023e0c21aadfd6af
Picobuu <steve.toffano@gmail.com>
2018-06-22 16:09:56 +0200

    Update

--- PHP-Objet/exemple-5/form/form.php
+++ PHP-Objet/exemple-5/form/form.php
@@ -7,19 +7,11 @@ class Form
 	private $action;
 	private $attr;
 
-	/**
-	 * Get the value of name
-	 */ 
 	public function getName()
 	{
 		return $this->name;
 	}
 
-	/**
-	 * Set the value of name
-	 *
-	 * @return  self
-	 */ 
 	public function setName($name)
 	{
 		$this->name = $name;
@@ -27,19 +19,11 @@ class Form
 		return $this;
 	}
 
-	/**
-	 * Get the value of method
-	 */ 
 	public function getMethod()
 	{
 		return $this->method;
 	}
 
-	/**
-	 * Set the value of method
-	 *
-	 * @return  self
-	 */ 
 	public function setMethod($method)
 	{
 		$this->method = $method;
@@ -47,40 +31,24 @@ class Form
 		return $this;
 	}
 
-	/**
-	 * Get the value of action
-	 */ 
 	public function getAction()
 	{
 		return $this->action;
 	}
 
-	/**
-	 * Set the value of action
-	 *
-	 * @return  self
-	 */ 
 	public function setAction($action)
 	{
 		$this->action = $action;
 
 		return $this;
 	}
-
-	/**
-	 * Get the value of attr
-	 */ 
+ 
 	public function getAttr()
 	{
 		return $this->attr;
 	}
 
-	/**
-	 * Set the value of attr
-	 *
-	 * @return  self
-	 */ 
-	public function setAttr($attr)
+	public function setAttr(array $attr)
 	{
 		$this->attr = $attr;
 
