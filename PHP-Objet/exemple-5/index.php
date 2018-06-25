1b2ff3d95116448c81f27cbe94153c7c11216e5c
Picobuu <steve.toffano@gmail.com>
2018-06-25 14:33:53 +0200

    Update

--- PHP-Objet/exemple-5/index.php
+++ PHP-Objet/exemple-5/index.php
@@ -2,10 +2,26 @@
 
 require_once('Include/header.php');
 
-require_once('Form/Form.php');
+/* require_once('Form/Form.php');
 require_once('Form/TextItem.php');
 require_once('Form/SelectItem.php');
-require_once('Entity/User.php');
+require_once('Entity/User.php'); */
+
+
+// Ne plus utiliser les require_once
+spl_autoload_register( function($className)
+{
+	if(file_exists('Entity/'.$className.'.php'))
+	{
+		require_once('Entity/'.$className.'.php');
+	}
+	elseif(file_exists('Form/'.$className.'.php'))
+	{
+		require_once('Form/'.$className.'.php');
+	}
+	echo "Classe appelée: ".$className;
+});
+
 
 $user = new User("Piote", "Azerty", "superemail@gmail.com", "IT");
 // echo $user->getPassword();
