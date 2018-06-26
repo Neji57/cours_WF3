138986a67c4f0f1ed21c4ef2f5911c5a34b42fd6
Picobuu <steve.toffano@gmail.com>
2018-06-26 15:09:13 +0200

    Update

--- PHP-Objet/exemple-7/Article.php
+++ PHP-Objet/exemple-7/Article.php
@@ -2,13 +2,63 @@
 
 class Article
 {
+	private $name = "Nom";
+
+	public function getData()
+	{
+		return "DATA";
+	}
+	// Appelée lors de l'instanciation
 	public function __construct()
 	{
 		echo "Object Article créée <hr>";
 	}
 
+	// Appel"e lors de la dectruction (fin du script)
 	public function __destruct()
 	{
 		echo "Object Article détruit <hr>";
+	}
+
+	// Appelée lors de l'appel d'une méthode
+	public function __call($name, $attr)
+	{
+		if(strlen($name) == "getData")
+		{
+			trigger_error("La méthode " . $name . " n'existe pas, vouliez-vous dire getDate ?");
+		}
+		else
+		{
+			echo "Méthode " . $name . " appelée <hr>";
+		}
+	}
+
+	// Pour une méthode statique
+	public static function __callstatic($name, $attr)
+	{
+		echo "Méthode statique " . $name . " appelée <hr>";
+	}
+
+	// Appelée lors de la lecture d'une propriété inaccessible
+	public function __get($name)
+	{
+		echo "Propriété " . $name . " est appelée </br>";
+		$method = 'get' . ucfirst($name);
+		if(method_exists($this, $method))
+		{
+			return $this -> method();
+		}
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setName($name)
+	{
+		$this->name = $name;
+
+		return $this;
 	}
 }
--- PHP-Objet/exemple-7/index.php
+++ PHP-Objet/exemple-7/index.php
@@ -3,3 +3,7 @@
 require_once('Article.php');
 
 $article = new Article;
+$article -> getDate();
+Article::getCounter();
+echo $article -> name;
+echo '</br>';
