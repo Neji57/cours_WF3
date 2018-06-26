2dd340a12ac2ee983ec881e8e4ff94dd5ae28c80
Picobuu <steve.toffano@gmail.com>
2018-06-26 09:26:02 +0200

    Update

--- PHP-Objet/exemple-6/Voiture.php
+++ PHP-Objet/exemple-6/Voiture.php
@@ -5,12 +5,16 @@ require_once('Vehicule.php');
 class Voiture extends Vehicule
 {
 
-    public function __construct
+    public function __construct($couleur)
+    {
+        parent::__construct($couleur);
+        $this -> type = "Voiture";
+    }
 
     public function toString()
     {
         return "Voiture " . parent::toString();
-        $this->type = "Voiture";
+        $this -> type = "Voiture";
     }
 
     public static function klaxonner()
--- PHP-Objet/exemple-6/index.php
+++ PHP-Objet/exemple-6/index.php
@@ -5,9 +5,11 @@ require_once('Voiture.php');
 
 $vehicule = new Vehicule("#990000");
 // $vehicule->setCouleur();
-echo $vehicule->toString();
+echo $vehicule -> toString();
 
 $voiture = new Voiture("#890000");
+echo '<hr>';
 echo $voiture -> toString();
-$voiture->setNombrePortes(4);
-Voiture::klaxoner();
+echo '<hr>';
+$voiture -> setNombrePortes(4);
+Voiture::klaxonner();
