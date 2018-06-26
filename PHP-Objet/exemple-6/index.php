ba3c49c2ed22ca2f47d8558789c7e9fe5e8dc116
Picobuu <steve.toffano@gmail.com>
2018-06-26 10:25:52 +0200

    Update

--- PHP-Objet/exemple-6/index.php
+++ PHP-Objet/exemple-6/index.php
@@ -1,8 +1,19 @@
 <?php
 
-require_once('Vehicule.php');
+/* require_once('Vehicule.php');
 require_once('Voiture.php');
 require_once('Moto.php');
+ */
+
+// Ne plus utiliser les require_once
+spl_autoload_register( function($className)
+{
+	$className = str_replace('\\', '/', $className); // remplacer les \ par des /
+	if(file_exists($className.'.php'))
+	{
+		require_once($className.'.php');
+	}
+});
 
 $vehicule = new Vehicule("#990000");
 // $vehicule->setCouleur();
