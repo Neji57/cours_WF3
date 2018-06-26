3af7547c2f4565c42d65d23fd71cba256035ab86
Picobuu <steve.toffano@gmail.com>
2018-06-26 09:21:18 +0200

    Annulation des changements

--- PHP-Objet/exemple-6/Vehicule.php
+++ PHP-Objet/exemple-6/Vehicule.php
@@ -8,19 +8,21 @@ class Vehicule
     protected $couleur;
     protected $dateCreation;
     protected $type;
+    const STATUT_ENPANNE = 'en panne';
+    const STATUT_REPARE = 'réparé';
 
     public function __construct(string $couleur)
     {
-        $this->setCouleur($couleur);
-        $this->dateCreation = new DateTime;
-        $this->type ="Vehicule";
+        $this -> setCouleur($couleur);
+        $this -> dateCreation = new DateTime;
+        $this -> type = "Vehicule";
     }
 
     public function setCouleur(string $couleur)
     {
         if(strlen($couleur) == 7)
         {
-            $this->couleur = $couleur;
+            $this -> couleur = $couleur;
         }
         else
         {
@@ -30,22 +32,22 @@ class Vehicule
 
     public function getCouleur()
     {
-        return $this->couleur;
+        return $this -> couleur;
     }
 
     public function toString()
     {
-        return "Vehicule couleur " . $this->getCouleur();
+        return "Vehicule couleur " . $this -> getCouleur();
     }
 
     public function getNombrePortes()
     {
-        return $this->nombrePortes;
+        return $this -> nombrePortes;
     }
 
     public function setNombrePortes($nombrePortes)
     {
-        $this->nombrePortes = $nombrePortes;
+        $this -> nombrePortes = $nombrePortes;
 
         return $this;
     }
--- PHP-Objet/exemple-6/Voiture.php
+++ PHP-Objet/exemple-6/Voiture.php
@@ -4,9 +4,17 @@ require_once('Vehicule.php');
 
 class Voiture extends Vehicule
 {
+
+    public function __construct
+
     public function toString()
     {
         return "Voiture " . parent::toString();
         $this->type = "Voiture";
+    }
+
+    public static function klaxonner()
+    {
+        echo "pouet";
     }
 }
