b3043b861594ada8f101c332692a4649cc665fc2
Picobuu <steve.toffano@gmail.com>
2018-06-26 14:46:11 +0200

    Update

--- PHP-Objet/exemple-7/Article.php
+++ PHP-Objet/exemple-7/Article.php
@@ -2,5 +2,13 @@
 
 class Article
 {
-	
+	public function __construct()
+	{
+		echo "Object Article créée <hr>";
+	}
+
+	public function __destruct()
+	{
+		echo "Object Article détruit <hr>";
+	}
 }
