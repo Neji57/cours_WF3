94f021ff21d975279d760bcc19880df6b52ba96c
Picobuu <steve.toffano@gmail.com>
2018-06-26 12:31:55 +0200

    Update

--- PHP-Objet/exemple-5/form/TextareaItem.php
+++ PHP-Objet/exemple-5/form/TextareaItem.php
@@ -7,7 +7,7 @@ class TextareaItem extends FormItem
 	public function createView()
     {
         $html = $this->startView();
-        $html .= '<textarea class="form-control" name="' . $this->getName() . '"/>' . $this -> getValue() .'</textarea>';
+        $html .= '<textarea class="form-control" placeholder="' . $this -> getLabel() .'" name="' . $this->getName() . '"/>' . $this -> getLabel() .'</textarea>';
         $html .= $this->endView();
 
         return $html;
