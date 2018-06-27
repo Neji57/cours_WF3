8747740bc3c5c8c6a6b77a1fab4184868c157dfd
Picobuu <steve.toffano@gmail.com>
2018-06-27 12:31:33 +0200

    Update

--- PHP-Objet/exemple-8/index.php
+++ PHP-Objet/exemple-8/index.php
@@ -2,7 +2,7 @@
 
 // Autoload
 spl_autoload_register(function ($className) {
-	$className = str_replace('\\', '/', $className); // remplacer les \ par des /
+	$className = str_replace('\\', '/', $className); // remplacer les \ par des / mac OS X uniquement
 	if (file_exists($className . '.php')) {
 		require_once($className . '.php');
 	}
