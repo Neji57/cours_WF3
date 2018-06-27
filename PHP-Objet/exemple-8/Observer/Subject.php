8fed1db47583ec9d18090e3185ec0202c2511908
Picobuu <steve.toffano@gmail.com>
2018-06-27 12:06:31 +0200

    Update

--- PHP-Objet/exemple-8/Observer/Subject.php
+++ PHP-Objet/exemple-8/Observer/Subject.php
@@ -4,14 +4,21 @@ namespace Observer;
 
 class Subject implements \SplSubject
 {
+	private $observers = [];
+
 	/* Méthodes */
 	public function attach (\SplObserver $observer)
 	{
-
+		$this -> observers[] = $observer;
 	}
 	public function detach (\SplObserver $observer)
 	{
-
+		foreach ($this -> observers as $key => $value) {
+			if($value == $observer)
+			{
+				unset($this -> observers[$key]);
+			}
+		}
 	}
 	public function notify ()
 	{
