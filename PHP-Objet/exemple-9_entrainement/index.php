4d9d12119ddc325d5610b595b14bb813d8479887
Picobuu <steve.toffano@gmail.com>
2018-06-28 09:22:41 +0200

    Update

---
+++ PHP-Objet/exemple-9_entrainement/index.php
@@ -0,0 +1,64 @@
+<?php
+
+spl_autoload_register(function ($className) {
+    $className = str_replace('\\', '/', $className); // remplacer les \ par des / mac OS X uniquement
+    if (file_exists($className . '.php')) {
+        require_once($className . '.php');
+    }
+});
+
+require_once('inc/header.php');
+
+use \Maison\Maison;
+use \Maison\Piece;
+
+$maison = new Maison("Bois", "Tuiles");
+// $maison -> setMateriauxStructure("Briques");
+
+$chambre1 = new Piece("Chambre 1", 16, 2.5);
+$cuisine = new Piece("Cuisine", 20, 2.5);
+
+$pieces = [$chambre1, $cuisine];
+$maison->setPieces($pieces);
+
+// echo $maison -> getMateriauxStructure();
+// echo '<hr/>';
+// echo $maison -> getMateriauxToiture();
+
+var_dump($maison->getInfos());
+$infos = $maison->getInfos();
+
+
+
+?>
+
+<div class="form-group">
+  <input type="text" class="form-control" name="nom" placeholder="Nom de la maison">
+</div>
+<div class="form-group">
+  <input type="text" class="form-control" name="matStructure" value"<?= $infos['matStructure'] ?>" placeholder="Matériaux de la structure">
+</div>
+<div class="form-group">
+  <input type="text" class="form-control" name="matToiture" value"<?= $infos['matToiture'] ?>" placeholder="Matériaux de la toiture">
+</div>
+<div class="form-group">
+    <textarea class="form-control" name="pieces" id="pieces" rows="3">
+
+<?php
+    foreach ($infos['pieces'] as $key => $value) {
+        echo "PIECES $key
+        ";
+        echo "Nom: " . $value['nom'] ."
+        ";
+        echo "Surface: " . $value['surface'] . "
+        ";
+        echo "Hauteur: " . $value['hauteur'] . "
+        ";
+        echo "Nombre de fenêtre: " . $value['nbFenetres'] . "
+            ";
+    }
+?>
+    </textarea>
+</div>
+
+<?php require_once('inc/footer.php');?>
