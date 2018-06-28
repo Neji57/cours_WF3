fd922af1239cbbf88aa0cd710c75f6526651dcf5
Picobuu <steve.toffano@gmail.com>
2018-06-28 13:14:05 +0200

    Update

--- PHP-Objet/Evaluation/index.php
+++ PHP-Objet/Evaluation/index.php
@@ -3,7 +3,6 @@
 require_once('inc/header.php');
 
 use Autoloader\Autoloader;
-use Form\Form;
 
 // Requiert la classe AUtoloader
 require_once('Autoloader/Autoloader.php');
@@ -11,14 +10,7 @@ require_once('Autoloader/Autoloader.php');
 // Appelle la fonction register de la classe Autoloader
 Autoloader::register();
 
-$form = new Form($_POST);
 
-echo $form->input("username");
-echo $form->input("password");
-echo $form->submit();
-
-$multi = [[2 => 'deux', '3', 'quatre'], [5, 6, 7], ['x', 'y', 'z']];
-echo $multi [0][3] + $multi[1][2] . $multi[2][1+1];
 
 
 // Appel de la page header
