3cd18899f90ae2e93f76c1e68da2c02bad4f5a2f
Picobuu <steve.toffano@gmail.com>
2018-07-03 15:22:29 +0200

    Update

--- PHP-Symphony/MVCExemple/index.php
+++ PHP-Symphony/MVCExemple/index.php
@@ -15,6 +15,13 @@ $loader = new Twig_Loader_Filesystem(__DIR__ . '/src/View');
 $twig = new Twig_Environment($loader, array('debug' => true));
 $twig->addGlobal('path', BASEPATH);
 
+// Création filtre
+$filter = new Twig_Filter('icon', function($text){
+    return preg_replace_callback('', function($matches){
+
+    }, $text)
+});
+
 use Controller\FrontController;
 
 $frontController = new FrontController();
