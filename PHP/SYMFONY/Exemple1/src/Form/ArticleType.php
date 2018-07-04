327e0e5f203b8c861fbe61fa54f548f7a200d9a9
Picobuu <steve.toffano@gmail.com>
2018-07-04 15:39:58 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Form/ArticleType.php
+++ PHP/SYMFONY/Exemple1/src/Form/ArticleType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleType extends AbstractType
 {
@@ -13,6 +14,9 @@ class ArticleType extends AbstractType
 		$builder
 			->add('title')
 			->add('content')
+			->add('save', SubmitType::class, array(
+
+			))
 		;
 	}
 }
