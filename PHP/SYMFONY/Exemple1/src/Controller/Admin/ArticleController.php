ab74eef2df7f2d706c3b78564a313080ece3731b
Picobuu <steve.toffano@gmail.com>
2018-07-04 14:09:52 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
+++ PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
@@ -3,11 +3,25 @@
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends Controller
 {
+	/**
+	 *
+	 * @Route ('/admin/article')
+	 */
 	public function index()
 	{
 		return $this->render('admin/article/index.html.twig');
+	}
+
+	/**
+	 *
+	 * @Route ('/admin/article')
+	 */
+	public function new()
+	{
+		return $this->render('admin/article/new.html.twig');
 	}
 }
