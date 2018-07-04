1fdb7bf77b923b9e47ed4e40e064d27c7c1b4aa6
Picobuu <steve.toffano@gmail.com>
2018-07-04 12:59:04 +0200

    Update

--- PHP/SYMFONY/Exemple1/assets/JS/app.js
+++ PHP/SYMFONY/Exemple1/assets/JS/app.js
@@ -1,3 +1,1 @@
 import 'bootstrap';
-
-$('body').append('HELLO JQUERY');
--- PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
+++ PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
@@ -8,7 +8,6 @@ class ArticleController extends Controller
 {
 	public function index()
 	{
-		var_dump('OK');
 		return $this->render('admin/article/index.html.twig');
 	}
 }
