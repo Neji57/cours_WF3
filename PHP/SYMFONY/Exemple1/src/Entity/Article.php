87ec5671372abf3e44f82a7bb222c34fbe32be2d
Picobuu <steve.toffano@gmail.com>
2018-07-04 14:37:12 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Entity/Article.php
+++ PHP/SYMFONY/Exemple1/src/Entity/Article.php
@@ -3,3 +3,116 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mappping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+
+ class Article
+ {
+	 /**
+	  *
+	  * @ORM\Id
+	  *@ORM\GeneratedValue
+	  */
+	private $id;
+	private $title;
+	private $content;
+	private $dateCreate;
+	private $dateUpdate;
+
+	public function __construct()
+	{
+		$this->dateCreate = new \DateTime;
+		$this->dateUpdate = new \DateTime;
+	}
+
+	/**
+	 * Get the value of id
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+
+	/**
+	 * Get the value of title
+	 */
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	/**
+	 * Set the value of title
+	 *
+	 * @return  self
+	 */
+	public function setTitle($title)
+	{
+		$this->title = $title;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of content
+	 */
+	public function getContent()
+	{
+		return $this->content;
+	}
+
+	/**
+	 * Set the value of content
+	 *
+	 * @return  self
+	 */
+	public function setContent($content)
+	{
+		$this->content = $content;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of dateCreate
+	 */
+	public function getDateCreate()
+	{
+		return $this->dateCreate;
+	}
+
+	/**
+	 * Set the value of dateCreate
+	 *
+	 * @return  self
+	 */
+	public function setDateCreate($dateCreate)
+	{
+		$this->dateCreate = $dateCreate;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of dateUpdate
+	 */
+	public function getDateUpdate()
+	{
+		return $this->dateUpdate;
+	}
+
+	/**
+	 * Set the value of dateUpdate
+	 *
+	 * @return  self
+	 */
+	public function setDateUpdate($dateUpdate)
+	{
+		$this->dateUpdate = $dateUpdate;
+
+		return $this;
+	}
+ }
