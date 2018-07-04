6dff05f5ed342550aa78b028f5de98d28d488d77
Picobuu <steve.toffano@gmail.com>
2018-07-04 15:52:01 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Form/ArticleType.php
+++ PHP/SYMFONY/Exemple1/src/Form/ArticleType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+//use Symfony\Component\Form\Extension\Core\Type\CountryType;
 
 class ArticleType extends AbstractType
 {
@@ -12,10 +13,15 @@ class ArticleType extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
-			->add('title')
+			->add('title', null, array(
+				'label' => 'Titre'
+			))
 			->add('content', null, array(
 				'label' => 'Contenu'
 			))
+			// ->add('country', CountryType::class, array(
+			// 	'mapped' =>false,
+			// ))
 			->add('save', SubmitType::class, array(
 				'label' => 'Enregistrer'
 			))
