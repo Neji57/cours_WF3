5627b9101ecd019e413d37b303cc9f1c134d7286
Picobuu <steve.toffano@gmail.com>
2018-07-05 14:11:52 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
+++ PHP/SYMFONY/Exemple1/src/Controller/Admin/ArticleController.php
@@ -4,28 +4,37 @@ namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+
 use Symfony\Component\HttpFoundation\Request;
+
 use App\Entity\Article;
 use App\Form\ArticleType;
 
 /**
  * @Route("/admin/article")
  */
+
 class ArticleController extends Controller
 {
 	/**
-	 * @Route("/{page}", requirements={"page" = "\d+"}, defaults={"page"=1})
+	 * @Route("/{page}", requirements={"page" = "\d+"}, defaults={"page" = 1})
 	 */
 	public function index($page)
 	{
 		$count = 10;
+
 		$em = $this->getDoctrine()->getManager();
-		$entities = $em->getRepository(Article::class)->findByPage($page, $count);
+
+		$entities = $em
+			->getRepository(Article::class)
+			->findByPage($page, $count);
+
 		$nbPages = ceil(count($entities) / $count);
+
 		return $this->render('admin/article/index.html.twig', array(
 			'entities' => $entities,
-			'nbPages' => $nbPages,
-			'page' => $page,
+			'nbPages' => (int)$nbPages,
+			'page' => $page
 		));
 	}
 
@@ -34,15 +43,14 @@ class ArticleController extends Controller
 	 */
 	public function new(Request $request)
 	{
+		// NOUVELLE ENTITÉE ARTICLE
 		$article = new Article;
 
-		// Création du formulaire
+		// CREATION DU FORMULAIRE
 		$form = $this->createForm(ArticleType::class, $article);
 		$form->handleRequest($request);
 
-		if($form->isSubmitted() && $form->isValid())
-		{
-				// Sauvegarde dans la BDD
+		if ($form->isSubmitted() && $form->isValid()) {
 			$em = $this->getDoctrine()->getManager();
 			$em->persist($article);
 			$em->flush();
@@ -50,45 +58,27 @@ class ArticleController extends Controller
 			return $this->redirectToRoute('app_admin_article_index');
 		}
 
-		// if(!empty($_POST))
-		// {
-		// 	$post = array();
-		// 	foreach($_POST as $key => $p)
-		// 	{
-		// 		$post[$key] = trim($p);
-		// 	}
-		// 	$article
-		// 		->setTitle(strip_tags($_POST['title']))
-		// 		->setContent($post['content'])
-		// 	;
-
-		// 	// Sauvegarde dans la BDD
-		// 	$em = $this->getDoctrine()->getManager();
-		// 	$em->persist($article);
-		// 	$em->flush();
-		// }
-
 		return $this->render('admin/article/new.html.twig', array(
-			'form' =>$form->createView(),
+			'form' => $form->createView(),
 		));
 	}
 
 	/**
-	 * @Route("/edit/{id}", requirements={"id" = "\d+"}) void
+	 * @Route ("/edit/{id}", requirements={"id" = "\d+"})
 	 */
 	public function edit(Request $request, Article $article)
 	{
-		// Création du formulaire
+		// CREATION DU FORMULAIRE
 		$form = $this->createForm(ArticleType::class, $article);
 		$form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid())
-		{
-				// Sauvegarde dans la BDD
+		if ($form->isSubmitted() && $form->isValid()) {
 			$article->setDateUpdate(new \DateTime);
 			$em = $this->getDoctrine()->getManager();
 			$em->persist($article);
 			$em->flush();
+
+			return $this->redirectToRoute('app_admin_article_index');
 		}
 
 		return $this->render('admin/article/edit.html.twig', array(
@@ -97,19 +87,19 @@ class ArticleController extends Controller
 	}
 
 	/**
-	 * @Route("/delete/{id}", requirements={"id" = "\d+"}) void
+	 * @Route ("/delete/{id}", requirements={"id" = "\d+"})
 	 */
 	public function delete(Request $request, Article $article)
 	{
 		$formBuilder = $this->createFormBuilder()
 			->setAction($this->generateUrl('app_admin_article_delete', ['id' => $article->getId()]))
-			->setMethod('DELETE')
-		;
+			->setMethod('DELETE');
 
 		$form = $formBuilder->getForm();
 
 		return $this->render('admin/article/delete.html.twig', array(
 			'form' => $form->createView(),
+			'entity' => $article
 		));
 	}
 }
