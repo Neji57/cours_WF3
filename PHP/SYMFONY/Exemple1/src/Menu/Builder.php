9c35708d611579b689a58c9cf6fa2b76247e1b8e
Picobuu <steve.toffano@gmail.com>
2018-07-05 16:05:15 +0200

    Update

---
+++ PHP/SYMFONY/Exemple1/src/Menu/Builder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+
+class Builder
+{
+	private $factory;
+
+	public function __construct(FactoryInterface $factory)
+	{
+		$this->factory = $factory;
+	}
+
+	public function createAdminMenu()
+	{
+		$menu = $this->factory->createItem('root');
+		return $menu;
+	}
+}
