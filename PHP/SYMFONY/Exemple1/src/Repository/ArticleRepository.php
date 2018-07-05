13098e0f9678b5df07647393e4eaa546e7b6c1da
Picobuu <steve.toffano@gmail.com>
2018-07-05 10:24:58 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Repository/ArticleRepository.php
+++ PHP/SYMFONY/Exemple1/src/Repository/ArticleRepository.php
@@ -5,11 +5,21 @@ namespace App\Repository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use App\Entity\Article;
+use Doctrine\ORM\Mapping\OrderBy;
 
 class ArticleRepository extends ServiceEntityRepository
 {
 	public function __construct(RegistryInterface $registry)
 	{
 		parent::__construct($registry, Article::class);
+	}
+
+	public function findByPage($page = 1, $count = 10)
+	{
+		$queryBuilder = $this->createQueryBuilder('a')
+			->OrderBy('a.dateCreate', 'ASC')
+			->setFirstResult($first)
+			->setMaxResults($count)
+		;
 	}
 }
