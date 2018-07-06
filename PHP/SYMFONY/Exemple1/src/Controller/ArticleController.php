888c2671e9d330db1dc04638023a6fd1da498ef6
Picobuu <steve.toffano@gmail.com>
2018-07-06 11:59:08 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Controller/ArticleController.php
+++ PHP/SYMFONY/Exemple1/src/Controller/ArticleController.php
@@ -16,5 +16,35 @@ use App\Form\ArticleType;
 
 class ArticleController extends Controller
 {
+	/**
+	 * @Route("/{page}", requirements={"page" = "\d+"}, defaults={"page" = 1})
+	 */
+	public function index($page)
+	{
+		$count = 10;
+
+		$em = $this -> getDoctrine() -> getManager();
+
+		$entities = $em
+			-> getRepository(Article::class)
+			->findByPage($page, $count)
+		;
+
+		$nbPages = ceil(count($entities) / $count);
+
+		if($nbPages < $page)
+		{
+			$t = $this->get('translator');
+			$this->addFlash('danger', $t->transChoice('page_error', $nbPages,  array('%nbPages%' => $nbPages)));
+			return $this->redirectToRoute('app_admin_article_index');
+		}
+
+		return $this->render('admin/article/index.html.twig', array(
+			'entities' => $entities,
+			'nbPages' => (int)$nbPages,
+			'page' => $page
+		));
+	}
+
 
 }
