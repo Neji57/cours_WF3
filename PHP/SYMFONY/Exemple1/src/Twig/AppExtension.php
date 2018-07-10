a960d1042131a7f264a820ae941087bfeba226ff
Picobuu <steve.toffano@gmail.com>
2018-07-10 11:04:27 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Twig/AppExtension.php
+++ PHP/SYMFONY/Exemple1/src/Twig/AppExtension.php
@@ -11,14 +11,14 @@ class AppExtension extends AbstractExtension
     public function getFilters(): array
     {
         return [
-            new TwigFilter('filter_name', [$this, 'doSomething'], ['is_safe' => ['html']]),
+            new TwigFilter('filter_name', [$this, 'parseIcons'], ['is_safe' => ['html']]),
         ];
     }
 
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('function_name', [$this, 'doSomething']),
+
         ];
     }
 
