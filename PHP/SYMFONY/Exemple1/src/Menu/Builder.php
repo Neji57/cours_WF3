17a66be6012bed2d55372fbd1c98a40bedca60ce
Picobuu <steve.toffano@gmail.com>
2018-07-10 11:44:07 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Menu/Builder.php
+++ PHP/SYMFONY/Exemple1/src/Menu/Builder.php
@@ -17,10 +17,15 @@ class Builder
 	{
 		$menu = $this->factory->createItem('root');
 		$menu->setChildrenAttribute('class', 'navbar-nav');
-		$menu->addChild('article.article', ['route' => 'app_admin_article_index']);
-		$menu->addChild('article.add', ['route' => 'app_admin_article_new']);
-		$menu->addChild('category.category', ['route' => 'app_admin_category_index']);
-		$menu->addChild('category.add', ['route' => 'app_admin_category_new']);
+
+		$articleParent = $menu->addChild('article.article', ['route' => 'app_admin_article_index']);
+		$articleParent->addChild('article.articles', ['route' => 'app_admin_article_index']);
+		$articleParent->addChild('article.add', ['route' => 'app_admin_article_new']);
+
+		$categoryParent = $menu->addChild('category.categories', ['route' => 'app_admin_category_index']);
+		$categoryParent->addChild('category.categories', ['route' => 'app_admin_category_index']);
+		$categoryParent->addChild('category.add', ['route' => 'app_admin_category_new']);
+
 		$menu->addChild('user.user', ['route' => 'app_article_index']);
 		return $menu;
 	}
