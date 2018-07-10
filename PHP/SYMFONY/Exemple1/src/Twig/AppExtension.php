85b5b3b1887edef28f1912b8ebb116abd8ee4fd1
Picobuu <steve.toffano@gmail.com>
2018-07-10 11:08:08 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Twig/AppExtension.php
+++ PHP/SYMFONY/Exemple1/src/Twig/AppExtension.php
@@ -11,7 +11,8 @@ class AppExtension extends AbstractExtension
     public function getFilters(): array
     {
         return [
-            new TwigFilter('filter_name', [$this, 'parseIcons'], ['is_safe' => ['html']]),
+            new TwigFilter('parse_icons', [$this, 'parseIcons'], ['is_safe' => ['html']]),
+            new TwigFilter('slugify', [$this, 'slugify']),
         ];
     }
 
@@ -25,5 +26,27 @@ class AppExtension extends AbstractExtension
     public function doSomething($value)
     {
         return preg_replace('/\.icon-([a-z0-9-]+)/', '<i class="fa fa-$1"></i>', $text);
+    }
+
+    public function slugify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+        // trim
+        $text = trim($text, '-');
+        // remove duplicate -
+        $text = preg_replace('~-+~', '-', $text);
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
     }
 }
