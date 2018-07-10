e8f1ba6cbb7c35179f9030523d6d341b914319b8
Picobuu <steve.toffano@gmail.com>
2018-07-10 10:15:19 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Menu/Builder.php
+++ PHP/SYMFONY/Exemple1/src/Menu/Builder.php
@@ -23,4 +23,15 @@ class Builder
 		$menu->addChild('category.add', ['route' => 'app_admin_category_new']);
 		return $menu;
 	}
+
+	public function createMainMenu()
+	{
+		$menu = $this->factory->createItem('root');
+		$menu->setChildrenAttribute('class', 'navbar-nav');
+		$menu->addChild('article.article', ['route' => 'app_article_index']);
+		$menu->addChild('category.category', ['route' => 'app_category_index']);
+		return $menu;
+	}
+
+
 }
