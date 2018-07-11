1ebbd50450f1b2315316af6fd4933b102524bd24
Picobuu <steve.toffano@gmail.com>
2018-07-11 09:42:28 +0200

    Update

--- PHP/SYMFONY/Exemple1/assets/JS/app.js
+++ PHP/SYMFONY/Exemple1/assets/JS/app.js
@@ -1,1 +1,12 @@
 import 'bootstrap';
+$(function () {
+
+	var $form = $('form[name="form"]');
+	$form.submit(function () {
+		$.post($form.attr('action'), $form.serialize(), function (data) {
+			console.log(data);
+
+		}, 'json')
+	})
+
+});
