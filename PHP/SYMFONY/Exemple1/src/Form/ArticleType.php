71898cef243b1f610eb366189b9decc948a12eda
Picobuu <steve.toffano@gmail.com>
2018-07-11 16:55:31 +0200

    Update

--- PHP/SYMFONY/Exemple1/src/Form/ArticleType.php
+++ PHP/SYMFONY/Exemple1/src/Form/ArticleType.php
@@ -4,41 +4,41 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Doctrine\ORM\EntityRepository;
-use Symfony\ComponentForm\extension\ChoiceType;
-//use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class ArticleType extends AbstractType
 {
-
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
 			->add('title', null, array(
 				'label' => 'Titre'
 			))
-			->add('categories', null, array(
+
+			->add('categories', null, array( // EntityType
 				'label' => 'category.category',
-				'expanded'=>true,
-				'required'=>true,
-				'query_builder'=>function(EntityRepository $er){
+				'choice_label' => 'name',
+				'expanded' => true,
+				'expanded' => true,
+				'query_builder' => function (EntityRepository $er) {
 					return $er->createQueryBuilder('c')
-						//->where('c.name', 'PHP');
 						->orderBy('c.name', 'ASC');
-				},
+				}
 			))
+
 			->add('image', ImageType::class)
+
+			->add('deleteImage', CheckboxType::class, array(
+                // 'mapped' => false,
+				'required' => false
+			))
+
 			->add('content', null, array(
 				'label' => 'Contenu',
 				'attr' => array(
-					'class' =>'tinymce',
+					'class' => 'tinymce'
 				)
-			))
-			// ->add('country', CountryType::class, array(
-			// 	'mapped' =>false,
-			// ))
-
-		;
+			));
 	}
 }
